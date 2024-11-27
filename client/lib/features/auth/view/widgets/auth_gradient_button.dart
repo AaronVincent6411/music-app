35d452fd068a397cad2fba1a788a931ffe859cf5
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2024-11-27 18:36:04 +0530

    added styling to sign up and sign in button

--- client/lib/features/auth/view/widgets/auth_gradient_button.dart
+++ client/lib/features/auth/view/widgets/auth_gradient_button.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:musicapp/core/theme/app_pallete.dart';
 
 class AuthGradientButton extends StatelessWidget {
-  const AuthGradientButton({super.key});
+  final String buttonText;
+  final VoidCallback onTap;
+  const AuthGradientButton({
+    super.key, 
+    required this.buttonText,
+    required this.onTap,
+    });
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      child: Text('Sign Up'),
-      );
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Pallete.gradient1,
+            Pallete.gradient2,
+          ],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.circular(7),
+      ),
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Pallete.transparentColor,
+          shadowColor: Pallete.transparentColor,
+          fixedSize: const Size(395, 50)
+        ),
+        child: Text(
+          buttonText,
+          style: const TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.w600,
+          ),
+          ),
+        ),
+    );
   }
 }
