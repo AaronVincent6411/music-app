080ca1de2574213c49b918d4974b16dc9afc7d9e
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2024-11-28 18:39:46 +0530

    created a login functionality

---
+++ client/lib/features/auth/view/pages/login_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:musicapp/core/theme/app_pallete.dart';
+import 'package:musicapp/features/auth/view/widgets/auth_gradient_button.dart';
+import 'package:musicapp/features/auth/view/widgets/custom_field.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController  = TextEditingController();
+  final passwordController  = TextEditingController();
+  final formkey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign In.',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  ),
+              ),
+              const SizedBox(height: 15),
+              CustomField(
+                hintText: 'Email',
+                controller: emailController,
+                ),
+              const SizedBox(height: 15),
+              CustomField(
+                hintText: 'Password',
+                controller: passwordController,
+                isObsecureText: true,
+                ),
+              const SizedBox(height: 20),
+                AuthGradientButton(
+                  buttonText: 'Sign In',
+                  onTap: () {
+                    
+                  },
+                ),
+              const SizedBox(height: 20),
+              RichText(text: TextSpan(
+                text: 'Don\'t have an account? ',
+                style: Theme.of(context).textTheme.titleMedium,
+                children: const [
+                  TextSpan(
+                    text: 'Sign Up',
+                    style: TextStyle(
+                      color: Pallete.gradient2,
+                      fontWeight: FontWeight.bold,
+                    )
+                    ),
+                ]
+              ))
+          
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
